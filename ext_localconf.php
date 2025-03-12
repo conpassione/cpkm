b023b10c8123b5db1e100c8aab864d8f982221cf
Renzo Bauen <info@conpassione.ch>
2025-03-12 16:30:08 +0100

    forms integrated

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,4 +2,17 @@
 declare(strict_types=1);
 defined('TYPO3') or die();
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 // call_user_func(static function(): void {});
+ExtensionManagementUtility::addTypoScriptSetup(
+    '
+   module.tx_form {
+       settings {
+           yamlConfigurations {
+               36651 = EXT:cpkm/Configuration/Form/CpFormSetup.yaml
+           }
+       }
+   }
+'
+);
